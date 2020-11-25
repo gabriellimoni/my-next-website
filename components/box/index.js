45ddc0bab64c83e179ea610ba7b6c96392a72333
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-11-24 23:07:28 -0300

    box component fix

--- components/box/index.js
+++ components/box/index.js
@@ -1,11 +1,13 @@
 import styles from './box.module.scss'
 
-export default function Box({ title, children }) {
+export default function Box({ title, children, width }) {
+    width = width || 'initial'
+    const upperCaseTitle = title.toUpperCase()
     return (
-        <div className={styles.box}>
-            <h1 className={styles.boxTitle}>{title}</h1>
+        <div className={styles.box} style={{ width }}>
+            <h1 className={styles.boxTitle}>{upperCaseTitle}</h1>
             
-            <p>{children}</p>
+            <div className={styles.boxContent}>{children}</div>
         </div>
     )
 }
